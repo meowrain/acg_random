555ad3eff1e6751a13cd0e8527d1f6ccaa4d8bf9
meowrain <meowrain@126.com>
2023-10-25 19:27:58 +0800

    feat: 添加文件上传功能

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Request, Query
+from fastapi import FastAPI, Request, Query, UploadFile
 from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
+from typing import Optional
 from fastapi.openapi.docs import (
     get_redoc_html,
     get_swagger_ui_html,
@@ -11,9 +12,21 @@ import os
 import random
 import datetime
 import re
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import uuid
+from PIL import Image
 app = FastAPI(title="Random ACG Pictures", version='1',
-              summary="一个简单得随机图API，Created by meowrain", description="欢迎来到Meowrain得随机图API网站，下面是API请求的使用方法",docs_url=None, redoc_url=None)
+              summary="一个简单得随机图API，Created by meowrain", description="欢迎来到Meowrain得随机图API网站，下面是API请求的使用方法", docs_url=None, redoc_url=None)
 
+# 设置取消跨域请求
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # 存储图片文件目录
 image_directory = "images"
 
@@ -25,7 +38,9 @@ app.mount('/static', StaticFiles(directory=image_directory), name="static")
 
 def getRandomFile(path):
     image_files = os.listdir(path)
-    random_image = random.choice(image_files)
+    filtered_image = [file for file in image_files if os.path.isfile(
+        os.path.join(path, file))]
+    random_image = random.choice(filtered_image)
     return random_image
 
 
@@ -39,6 +54,7 @@ def select_random_image():
         last_selected_day = current_day
     return last_selected_image
 
+
 @app.get("/docs", include_in_schema=False)
 async def custom_swagger_ui_html():
     return get_swagger_ui_html(
@@ -49,6 +65,7 @@ async def custom_swagger_ui_html():
         swagger_css_url="/static/css/swagger-ui.css",
     )
 
+
 @app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
 async def swagger_ui_redirect():
     return get_swagger_ui_oauth2_redirect_html()
@@ -57,8 +74,6 @@ async def swagger_ui_redirect():
 @app.get("/", description="返回到文档主页")
 async def home() -> RedirectResponse:
     return RedirectResponse("/docs")
-
-
 
 
 @app.get("/acg", description="获取随机图的url,json或者image格式返回")
@@ -95,7 +110,8 @@ async def get_random_image(request: Request, type: str = Query(default='image', 
         "info": "请修改请求类型"
     }
 
-@app.get('/sakura_acg')
+
+@app.get('/download')
 async def get_sakura_acg_random_images():
     sakura_url = "https://www.dmoe.cc/random.php?return=json"
     response = requests.get(sakura_url)
@@ -117,3 +133,29 @@ async def get_sakura_acg_random_images():
         return {
             "downloadinfo": "failed"
         }
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    allowed_extensions = ["txt", "pdf", "png", "jpg"]
+    ext = file.filename.split(".")[-1]
+    if ext not in allowed_extensions:
+        return {"error": "Unsupported file type"}
+    safe_filename = str(uuid.uuid4()) + "." + ext
+    file_location = os.path.join('upload', safe_filename)
+    try:
+        # 逐块读取和写入文件
+        with open(file_location, "wb+") as file_object:
+            while True:
+                chunk = await file.read(8192)  # 使用适当的缓冲区大小
+                if not chunk:
+                    break
+                file_object.write(chunk)
+    except OSError as e:
+        return {"error": f"Failed to save file: {str(e)}"}
+
+
+    return {"filename": safe_filename, "file_size": f'{ "%.2f" % (file.size/(1024**2))}MB','saved_position': file_location}
+
+if __name__ == '__main__':
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8888, reload=True)
