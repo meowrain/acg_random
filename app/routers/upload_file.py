642af5eea7d075f57c521af285422118a676507c
meowrain <meowrain@126.com>
2023-10-30 23:41:31 +0800

    feat:图片上传功能基本完成， 验证picgo成功

--- app/routers/upload_file.py
+++ app/routers/upload_file.py
@@ -1,10 +1,19 @@
-from fastapi import APIRouter,UploadFile
+from fastapi import APIRouter,UploadFile,Request,Body
+from typing import Annotated
 import uuid
 import os
+from dotenv import load_dotenv
 router = APIRouter(tags=["uploadImages"])
 
 @router.post("/uploadfile/")
-async def create_upload_file(image: UploadFile):
+async def create_upload_file(request: Request,token:Annotated[str, Body()],image: UploadFile):
+    load_dotenv()
+    set_token = os.getenv("pic_token")
+    if(token != set_token):
+        return {
+            "status": "error",
+            "info": "token error"
+        }
     allowed_extensions = ["png", "jpg","gif","webp","jpeg"]
     ext = image.filename.split(".")[-1]
     if ext not in allowed_extensions:
@@ -21,4 +30,4 @@ async def create_upload_file(image: UploadFile):
                 file_object.write(chunk)
     except OSError as e:
         return {"error": f"Failed to save file: {str(e)}"}
-    return {"filename": safe_filename, "file_size": f'{ "%.2f" % (image.size/(1024**2))}MB','saved_position': file_location,'url':''}
+    return {"filename": safe_filename, "file_size": f'{ "%.2f" % (image.size/(1024**2))}MB','saved_position': file_location,'url':request.url_for("upload", path=safe_filename)._url}
